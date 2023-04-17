a4f75a9a521be31cccb9e478a57455155407e18d
Satria Kamal Norsaid <satriakamaln@gmail.com>
2023-04-17 17:48:18 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function changeImageMerah(){
+    var img = document.getElementById('image');
+    img.src = "img/PETA KALSEL Merah.png";
+}
+function changeImageHijau(){
+    var img = document.getElementById('image');
+    img.src = "img/PETA KALSEL Hijau.png";
+}
+function changeImageBiru(){
+    var img = document.getElementById('image');
+    img.src = "img/PETA KALSEL Biru.png";
+}
